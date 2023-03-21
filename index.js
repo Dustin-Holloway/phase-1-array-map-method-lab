201cc9cc7260bd084d99610b5f98af642ecae28a
Dustin <dustinhollowaypdx@gmail.com>
2023-03-20 17:45:33 -0700

    almost there?

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(array) {
+  const emptyArray = [];
+
+  array.map((item) => {
+    const variable = item.split(" ");
+
+    const toCaps = variable.map((item) => {
+      return item[0].toUpperCase() + item.slice(1);
+    });
+    let newVariable = toCaps.join(" ");
+    return emptyArray.push(newVariable);
+  });
+  return emptyArray;
 }
+
+console.log(titleCased(tutorials));
+
+// function titleCased(array) {
+//   return array.map((item) => {
+//     return item.replace(/\b\w/g, (char) => char.toUpperCase());
+//   });
+// }
